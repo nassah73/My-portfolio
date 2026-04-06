555bd363636b5f15349789cac3ea6c5b463b5585
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-06 18:26:53 +0200

    porfolio profile

--- hassan-portfolio/src/App.jsx
+++ hassan-portfolio/src/App.jsx
@@ -1,5 +1,10 @@
+import Profile from './Profile.png'
 export default function Home(){
   return(
-    <h1 className="bg-black">hello world</h1>
+    <div className="bg-[#1f1f1f] w-screen h-screen">
+        <img src={Profile} alt="Profile" className='w-150  absolute right-0 top-[10%]' />
+        <div className="absolute inset-0 bg-gradient-to-r from-[#2a2a2a] via-[#000000]/50 to-transparent"></div>
+        <div className="absolute inset-0 bg-gradient-to-t from-[#000000] via-transparent to-transparent"></div>
+    </div>
   )
 }
