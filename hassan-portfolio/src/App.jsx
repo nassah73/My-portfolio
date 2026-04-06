9c28584839af71ba96c1d235fd5ed19bb56a61ce
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-06 17:51:45 +0200

    install taiwind

---
+++ hassan-portfolio/src/App.jsx
@@ -0,0 +1,5 @@
+export default function Home(){
+  return(
+    <h1 className="bg-black">hello world</h1>
+  )
+}
--- hassan-portfolio/src/main.jsx
+++ hassan-portfolio/src/main.jsx
@@ -1,5 +1,8 @@
-import {createRoot} from 'react-dom/client'
-const root =createRoot(document.getElementById('root'));
-root.render(
-    
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+ <App/>
 )
