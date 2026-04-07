a3741ea7e35f1d9aa3d93562a654b6d58788dae9
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-07 14:24:50 +0200

    content home

--- hassan-portfolio/src/App.jsx
+++ hassan-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import Profile from './Profile.png'
+import logo from './logo.png'
  const navBar=['home','About','Projects','Skills','Contact']
 export default function Home(){
   return(
@@ -6,6 +7,7 @@ export default function Home(){
         <img src={Profile} alt="Profile" className='w-150  absolute right-0 top-[10%] z-0' />
         <div className="absolute inset-0 bg-gradient-to-r from-[#2a2a2a] via-[#000000]/50 to-transparent z-0"></div>
         <div className="absolute inset-0 bg-gradient-to-t from-[#000000] via-transparent to-transparent z-0"></div>
+        <img src={logo} alt="logo" className='w-40 absolute ml-20'/>
         <nav className='absolute'>
           <ul className='flex justify-end w-screen '>
             {navBar.map((item,index)=>{
@@ -16,6 +18,12 @@ export default function Home(){
 
           </ul>
         </nav>
+        <div className=' absolute text-white top-1/3 left-20 leading-7 capitalize'>
+            <p >hi there !</p>
+            <p>i am <span className='text-[#10b981] font-[600]'>HASSAN ES-SEBAIY</span></p>
+             <h1>Full Stack & Software Developer specialized in Java, React, and Node.js.</h1>
+            <button>contact me </button>
+        </div>
     </div>
   )
 }
