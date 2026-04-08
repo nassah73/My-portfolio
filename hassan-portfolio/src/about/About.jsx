900d868189c126a5f9a53af7b432d0608d5dbe52
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-08 19:10:56 +0200

    img

--- hassan-portfolio/src/about/About.jsx
+++ hassan-portfolio/src/about/About.jsx
@@ -2,10 +2,12 @@ import Navbar  from "../Home/NavBar"
 export default function About(){
     return(
         <>
+        <div>
         <Navbar/>
         <div>
-            <div></div>
+            <div className="bg-[#10b981] w-100 h-100"></div>
             <img src="" alt="" />
+        </div>
         </div>
         </>
     )
