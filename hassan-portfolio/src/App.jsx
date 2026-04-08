c8d3763394c10916eeb0dd09b91ca877ff994ae4
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-08 17:09:06 +0200

    navbar file

--- hassan-portfolio/src/App.jsx
+++ hassan-portfolio/src/App.jsx
@@ -1,25 +1,16 @@
 import Profile from './Profile.png'
 import Typewriter from 'typewriter-effect';
 import { FaGithub } from 'react-icons/fa';
-import logo from './logo.png'
- const navBar=['home','About','Projects','Skills','Contact']
+
+import Navbar from './Home/NavBar'
+
 export default function Home(){
   return(
     <div className="bg-[#1f1f1f] w-screen h-screen z-0 relative">
         <img src={Profile} alt="Profile" className='w-150  absolute right-0 top-[10%] z-0' />
         <div className="absolute inset-0 bg-gradient-to-r from-[#2a2a2a] via-[#000000]/50 to-transparent z-0"></div>
         <div className="absolute inset-0 bg-gradient-to-t from-[#000000] via-transparent to-transparent z-0"></div>
-        <img src={logo} alt="logo" className='w-40 absolute ml-20'/>
-        <nav className='absolute'>
-          <ul className='flex justify-end w-screen '>
-            {navBar.map((item,index)=>{
-              return(
-                <li className='text-white mr-20 mt-5 hover:text-[#10b981] hover:cursor-pointer ' key={index}>{item}</li>
-              )
-            })}
-             <li><a href="https://github.com/nassah73"><FaGithub className='text-white size-7 mt-4.5 mr-5 hover:cursor-pointer hover:text-gray-400'/></a></li>
-          </ul>
-        </nav>
+        <Navbar/>
         <div className=' absolute text-white top-1/3 left-20 leading-7 capitalize font-[600] text-xl'>
                <Typewriter
                    onInit={(typewriter) => {
---
+++ hassan-portfolio/src/Home/NavBar.jsx
@@ -0,0 +1,20 @@
+import { FaGithub } from 'react-icons/fa';
+import logo from './logo.png'
+export default function Navbar(){
+     const navBar=['home','About','Projects','Skills','Contact']
+    return(
+        <>
+                <img src={logo} alt="logo" className='w-40 absolute ml-20'/>
+                <nav className='absolute'>
+                  <ul className='flex justify-end w-screen '>
+                    {navBar.map((item,index)=>{
+                      return(
+                        <li className='text-white mr-20 mt-5 hover:text-[#10b981] hover:cursor-pointer ' key={index}>{item}</li>
+                      )
+                    })}
+                     <li><a href="https://github.com/nassah73"><FaGithub className='text-white size-7 mt-4.5 mr-5 hover:cursor-pointer hover:text-gray-400'/></a></li>
+                  </ul>
+                </nav>
+    </>
+    )
+}
