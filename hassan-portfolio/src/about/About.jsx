76c16274e864d80a1598a9ae20f063802cb83f82
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-09 16:26:36 +0200

    styling

--- hassan-portfolio/src/about/About.jsx
+++ hassan-portfolio/src/about/About.jsx
@@ -1,12 +1,23 @@
 import Navbar  from "../Home/NavBar"
+import Img from './profile2.jpeg'
 export default function About(){
     return(
         <>
         <div>
         <Navbar/>
-        <div>
-            <div className="bg-[#10b981] w-100 h-80 rounded-xl"></div>
-            <img src="" alt="" />
+        <div className="">
+            <div className=" w-[1200px]  bg-amber-500 justify-self-center  flex  gap-10  relative top-[100px]">
+            <nav className="bg-amber-200 relative ">
+            <div className="bg-[#10b981] w-100 h-80 rounded-xl relative top-1/5 z-0 rotate-175"></div>
+            <img src={Img} alt="" className="w-100 h-80 object-cover rounded-xl absolute top-1/5 z-1 "/>
+             </nav>
+             <nav className="w-[100%] relative">
+                 <div cla></div>
+                  <p className="relative top-1/5 w-100 justify-self-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eveniet, reiciendis aut maxime impedit quos recusandae eius mollitia! Illo est blanditiis, mollitia laboriosam voluptates modi dolorum pariatur inventore commodi facilis.
+                  </p>
+             </nav>
+             </div>
         </div>
         </div>
         </>
