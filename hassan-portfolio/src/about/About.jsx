d9073fdceea0be46d11a17abefa0b939f45df98e
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-10 16:26:45 +0200

    content foter

--- hassan-portfolio/src/about/About.jsx
+++ hassan-portfolio/src/about/About.jsx
@@ -5,7 +5,7 @@ export default function About(){
         <>
         <div>
         <Navbar/>
-        <div className="">
+        <div className="w-[1200px]  justify-self-center relative h-screen">
             <div className=" w-[1200px]   justify-self-center  flex  gap-10  relative top-[100px]">
             <nav className=" relative w-[50%] ">
             <div className="bg-[#10b981] w-[100%] h-100 rounded-xl relative  z-0 rotate-175"></div>
@@ -22,7 +22,12 @@ export default function About(){
                   </p>
                   </nav>
              </nav>
+               
              </div>
+              <section className="grid grid-cols-5 grid-rows-2 absolute bottom-0 h- bg-amber-700 w-[100%]">
+                   <div className="bg-amber-300 h-20"> helo</div>
+                    <div className="bg-amber-300 h-20"> helo</div>
+                </section>
         </div>
         </div>
         </>
